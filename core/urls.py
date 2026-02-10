8e03641e151cb84bb505d6b7821f265ee93bf764
Romulo Nogueira <romulonogueira1607@gmail.com>
2026-02-10 20:58:10 -0300

    iniciado a criacao do html com django

--- core/urls.py
+++ core/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('api/docs', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('tasks.urls_html'))
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from rest_framework import viewsets
@@ -8,3 +8,29 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+    def task_list(request):
+        if request.method =='POST':
+            title =request.POST.get('title')
+
+            if title:
+                Task.objects.create(title=title)
+
+                return redirect('task_list')
+            tasks =Task.object.all().order_by('-created_at')
+            return render(request, 'tasks/task_list.html', {'tasks': tasks})
+        
+
+   #View para concluir a tarefa 
+    def task_complete(request, task_id):
+        task = Task.objects.get(id=task_id)
+        task.completed=True
+        task.save()
+        return redirect('task-list')
+    
+    # view para exlcuir tarefa
+    def task_delete(request, task_id):
+        task = Task.objects.get(id=task_id)
+        task.delete()
+        return redirect('task_list')
